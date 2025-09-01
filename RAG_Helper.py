36e4458a9f882bb7e9bfc10a12e3ebb0a8462fcc
chiya-chiu <chiya20041126@gmail.com>
2025-09-01 14:45:59 +0800

    Update RAG_Helper.py

--- RAG_Helper.py
+++ RAG_Helper.py
@@ -3,7 +3,8 @@ from pathlib import Path
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain.chains import create_retrieval_chain, create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from pydantic import PrivateAttr
 from langchain.schema import BaseRetriever, Document
